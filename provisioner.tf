5d3a3094c1a27865539b9720edb0b378d401e3a2
RaaviSivajiTech <ravi.siva1282@gmail.com>
2024-03-20 23:44:55 -0400

    changes

--- provisioner.tf
+++ provisioner.tf
@@ -166,4 +166,18 @@ resource "aws_instance" "web_server" {
   tags = {
     Name = var.web_server_name
   }
+
+  user_data = <<-EOF
+#!bin/bash
+    sudo yum update -y
+    sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+    sudo yum upgrade
+    sudo amazon-linux-extras install java-openjdk11 -y
+    sudo dnf install java-11-amazon-corretto -y
+    sudo yum install jenkins -y
+    sudo systemctl enable jenkins
+    sudo systemctl start jenkins
+  EOF
 }
+
